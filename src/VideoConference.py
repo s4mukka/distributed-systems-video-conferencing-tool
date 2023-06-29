6a3fa29b6f80dc3609d765f551386501fd3935c3
s4mukka <meninosam197@gmail.com>
2023-06-29 12:53:43 -0300

    atualiza video conference

--- src/VideoConference.py
+++ src/VideoConference.py
@@ -1,6 +1,7 @@
 from .Conference import Conference
 import cv2
 import numpy as np
+import base64
 import zmq
 
 class VideoConference(Conference):
@@ -11,18 +12,18 @@ class VideoConference(Conference):
         if ret:
             # Redimensiona o frame, se necessário
             frame = cv2.resize(frame, (640, 480))
+            encoded, buffer = cv2.imencode('.jpg', frame)
             # cv2.imshow("vid", frame)
 
             # Envia o frame como uma imagem JPEG
-            self.send_socket.send(frame.tobytes())
+            self.send_socket.send(base64.b64encode(buffer))
 
     def receive(self):
       while True:
         for receive_socket in self.receive_sockets:
-          frame_bytes = receive_socket.recv()
-          frame = np.frombuffer(frame_bytes, dtype=np.uint8)
-          frame = np.reshape(frame, (640, 480, 3))  # Restaura as dimensões do frame
+          frame = receive_socket.recv()
+          img = cv2.imdecode(np.fromstring(base64.b64decode(frame), dtype=np.uint8), 1)
           # Código para exibir o frame recebido
-          cv2.imshow(receive_socket.getsockopt(zmq.LAST_ENDPOINT).decode(), frame)
+          cv2.imshow(receive_socket.getsockopt(zmq.LAST_ENDPOINT).decode(), img)
           if cv2.waitKey(1) & 0xFF == ord('q'):  # Pressione 'q' para sair
               break
