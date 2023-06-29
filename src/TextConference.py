ac6d240bf73d7382957cde0a2149e4e436ddf457
s4mukka <meninosam197@gmail.com>
2023-06-29 11:40:40 -0300

    atualiza text conference

--- src/TextConference.py
+++ src/TextConference.py
@@ -3,11 +3,11 @@ from .Conference import Conference
 class TextConference(Conference):
     def send(self):
       while True:
-        message = input("Digite uma mensagem: ")
+        message = input("Digite uma mensagem: \n")
         self.send_socket.send_string(message)
 
     def receive(self):
       while True:
         for receive_socket in self.receive_sockets:
           message = receive_socket.recv_string()
-          print("Mensagem recebida:", message)
+          print(f"Mensagem recebida (receive_socket.getsockopt(zmq.LAST_ENDPOINT).decode()):", message)
